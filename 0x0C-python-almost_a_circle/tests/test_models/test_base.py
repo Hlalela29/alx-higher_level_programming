e63512405aa79bde6c565e81847c4965f28ea3f8
Hlalela29 <hlalelamasuluke@gmail.com>
2023-03-24 09:44:52 -0700

    test base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,3 +6,15 @@ Unit test for Base Class
 import os
 import unittest
 from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    """
+    Test for Base class
+    """
+
+    def test_docstring(self):
+        """
+        Test docstring
+        """
+        self.assertIsNotNone(Base.__doc__)
